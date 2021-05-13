0747918079b8ca1e5ccb3e7da222ec8a9535eeba
AcxelMorales <acxelmorales97@gmail.com>
2021-05-13 01:50:17 -0500

    Create a new context and reducer

--- src/HeroesApp.jsx
+++ src/HeroesApp.jsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 
+import { AuthContext } from './auth/AuthContext';
 import { AppRouter } from './routes/AppRouter';
 
+import { authReducer } from './auth/authReducer';
+
+const init = () => {
+  return JSON.parse(localStorage.getItem('user')) || {
+    logged: false
+  };
+};
+
 export const HeroesApp = () => {
-  return <AppRouter />;
+  const [user, dispatch] = useReducer(authReducer, {}, init);
+
+  return (
+    <AuthContext.Provider
+      value={{
+        user,
+        dispatch
+      }}
+    >
+      <AppRouter />
+    </AuthContext.Provider>
+  );
 };

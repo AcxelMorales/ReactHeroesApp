a91bcefd1985831dc59259b01dd76e0fe8714fb6
AcxelMorales <acxelmorales97@gmail.com>
2021-05-14 14:31:48 -0500

    Add private routes

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,18 +1,24 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import { PrivateRoute } from './PrivateRoute';
+
+import { AuthContext } from '../auth/AuthContext';
 
 import { LoginScreen } from '../components/auth/LoginScreen';
 import { DashboardRouter } from './DashboardRouter';
 
 export const AppRouter = () => {
+  const { user: { logged } } = useContext(AuthContext);
+
   return (
     <>
       <Router>
         <div>
           <Switch>
             <Route exact path="/login" component={LoginScreen} />
-            <Route path="/" component={DashboardRouter} />
+            <PrivateRoute isAuthenticated={logged} path="/" component={DashboardRouter} />
           </Switch>
         </div>
       </Router>
